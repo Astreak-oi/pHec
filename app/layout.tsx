6a5bd460dbaf8cf3fde0a1afa997aae93aa807b2
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-11-26 15:55:51 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,32 @@
-import { Metadata } from 'next';
-import '../styles/globals.css';
+import { Metadata } from "next";
+import "../styles/globals.css";
 
-const title = 'Get Idea Rating & Feedback By AI';
-const description = 'We&apos;ll analyze your idea and provide ratings, feedback and suggestions';
+const title = "Get Idea Rating & Feedback By AI";
+const description =
+  "We&apos;ll analyze your idea and provide ratings, feedback and suggestions";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://phec.vercel.app'),
+  metadataBase: new URL("https://phec.vercel.app"),
   title,
   description,
   openGraph: {
     title,
     description,
-    url: 'https://phec.vercel.app',
+    url: "https://phec.vercel.app",
     images: [
       {
-        url: 'https://phec.vercel.app/og',
+        url: "/og-image.png",
         width: 1200,
         height: 630,
-        alt: 'Phec',
+        alt: "Phec",
       },
     ],
-    locale: 'en_IN',
-    siteName: 'Phec',
-    type: 'website',
+    locale: "en_IN",
+    siteName: "Phec",
+    type: "website",
   },
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
     title,
     description,
   },
@@ -38,12 +39,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head><meta name="google-site-verification" content="6EVpVJ_CRnajBz5bw0UdB5_IFi-YPnpOtAyYqR9zHnQ" /></head>
-      <body>
-        {children}
-    
-      </body>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="6EVpVJ_CRnajBz5bw0UdB5_IFi-YPnpOtAyYqR9zHnQ"
+        />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
-
