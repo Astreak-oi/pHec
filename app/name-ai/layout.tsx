ca2c060e80c5ee0ff7d2694502d73add2c06b53c
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-12-28 16:47:46 +0530

    Refactor: code to improve performance and readability

---
+++ app/name-ai/layout.tsx
@@ -0,0 +1,56 @@
+import { Metadata } from "next";
+import "../styles/globals.css";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
+
+export const nameAiConfig = {
+  title: "Get Unique Name by AI",
+  description: "Generate Names for your Startup or Business",
+  url: "https://phec.astreak.in/name-ai",
+};
+
+export const metadata: Metadata = {
+  metadataBase: new URL(nameAiConfig.url),
+  title: {
+    default: nameAiConfig.title,
+    template: `%s - ${nameAiConfig.title}`,
+  },
+  description: nameAiConfig.description,
+  openGraph: {
+    title: nameAiConfig.title,
+    description: nameAiConfig.description,
+    url: nameAiConfig.url,
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: nameAiConfig.title,
+      },
+    ],
+    locale: "en_IN",
+    siteName: nameAiConfig.title,
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: nameAiConfig.title,
+    description: nameAiConfig.description,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
