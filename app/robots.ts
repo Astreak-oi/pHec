2037027f1b23e3bb4833be9cfe21f0c42533b7f1
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-12-28 16:30:12 +0530

    Fix bug in login functionality

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://phec.astreak.in/sitemap.xml',
+  }
+}
