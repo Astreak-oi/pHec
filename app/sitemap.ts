be7b0c1501b4a5790c2bee039f9aab5a5299d79e
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2024-11-19 01:40:21 +0530

    Remove changeFrequency property from sitemap entrie

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,36 +5,36 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://phec.astreak.in/",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      // changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: "https://phec.astreak.in/name-ai",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      // changeFrequency: "monthly",
       priority: 0.9,
     },
     {
       url: "https://phec.astreak.in/lp/best-software-solutions-company",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      // changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: "https://phec.astreak.in/lp/best-website-solutions-company",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      // changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: "https://phec.astreak.in/lp/best-ecommerce-solutions",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      // changeFrequency: "monthly",
       priority: 0.8,
     },{
       url: "https://phec.astreak.in/lp/crm-solutions-for-mid-size-businesses",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      // changeFrequency: "monthly",
       priority: 1,
     },
   ];
